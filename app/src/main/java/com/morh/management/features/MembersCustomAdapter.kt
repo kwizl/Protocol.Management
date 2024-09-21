af377fdbbf3d27cabde50b9ae4a9f3a393340b66
Martin Njoroge <martinn@innova.co.ke>
2024-09-21 13:24:09 +0300

    Implement copying contacts to clipboard for Members and Visitors

--- app/src/main/java/com/morh/management/features/MembersCustomAdapter.kt
+++ app/src/main/java/com/morh/management/features/MembersCustomAdapter.kt
@@ -1,19 +1,31 @@
 package com.morh.management.features
 
+import android.R.attr.name
 import android.annotation.SuppressLint
+import android.app.Application
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Context.*
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.RecyclerView
 import com.morh.management.models.Member
 import com.morh.protocolmanagement.R
 
+
 class MembersCustomAdapter(private var memberList : List<Member>?) : RecyclerView.Adapter<MembersCustomAdapter.MyViewHolder>() {
 
+    lateinit var context: Context
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_member, parent, false)
+        context = parent.context
+        val itemView = LayoutInflater.from(context).inflate(R.layout.list_member, parent, false)
         return MyViewHolder(itemView)
     }
 
@@ -30,6 +42,17 @@ class MembersCustomAdapter(private var memberList : List<Member>?) : RecyclerVie
         if (currentItem != null) {
             holder.textName.text = currentItem.Name
             holder.textContact.text = currentItem.Contact
+
+            holder.textContact.setOnLongClickListener(View.OnLongClickListener {
+                val clipboardManager = getSystemService(context, ClipboardManager::class.java) as ClipboardManager
+                val clipData = ClipData.newPlainText("Contact", holder.textContact.text)
+                clipboardManager.setPrimaryClip(clipData)
+
+                val toast = Toast.makeText(context, "Contact Copied", Toast.LENGTH_LONG)
+                toast.show()
+
+                true
+            })
         }
     }
 
@@ -38,7 +61,8 @@ class MembersCustomAdapter(private var memberList : List<Member>?) : RecyclerVie
     }
 
     class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
-        val textName : TextView = itemView.findViewById(R.id.Name)
-        val textContact : TextView = itemView.findViewById(R.id.Contact)
+        val textName : TextView = itemView.findViewById(R.id.Member_Name)
+        var textContact : TextView = itemView.findViewById(R.id.Member_Contact)
+
     }
 }
--- app/src/main/java/com/morh/management/features/VisitorsCustomAdapter.kt
+++ app/src/main/java/com/morh/management/features/VisitorsCustomAdapter.kt
@@ -1,20 +1,25 @@
 package com.morh.management.features
 
 import android.annotation.SuppressLint
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.RecyclerView
-import com.morh.management.models.Member
 import com.morh.management.models.Visitor
 import com.morh.protocolmanagement.R
 
 class VisitorsCustomAdapter(private var visitorList : List<Visitor>?) : RecyclerView.Adapter<VisitorsCustomAdapter.MyViewHolder>() {
 
+    lateinit var context: Context
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_visitor, parent, false)
+        context = parent.context
+        val itemView = LayoutInflater.from(context).inflate(R.layout.list_visitor, parent, false)
         return MyViewHolder(itemView)
     }
 
@@ -31,6 +36,17 @@ class VisitorsCustomAdapter(private var visitorList : List<Visitor>?) : Recycler
         if (currentItem != null) {
             holder.textName.text = currentItem.Name
             holder.textContact.text = currentItem.Contact
+
+            holder.textContact.setOnLongClickListener(View.OnLongClickListener {
+                val clipboardManager = getSystemService(context, ClipboardManager::class.java) as ClipboardManager
+                val clipData = ClipData.newPlainText("Contact", holder.textContact.text)
+                clipboardManager.setPrimaryClip(clipData)
+
+                val toast = Toast.makeText(context, "Contact Copied", Toast.LENGTH_LONG)
+                toast.show()
+
+                true
+            })
         }
     }
 
@@ -39,7 +55,7 @@ class VisitorsCustomAdapter(private var visitorList : List<Visitor>?) : Recycler
     }
 
     class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
-        val textName : TextView = itemView.findViewById(R.id.Name)
-        val textContact : TextView = itemView.findViewById(R.id.Contact)
+        val textName : TextView = itemView.findViewById(R.id.Visitor_Name)
+        val textContact : TextView = itemView.findViewById(R.id.Visitor_Contact)
     }
 }
